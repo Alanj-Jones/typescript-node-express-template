b90028a593f1e76c191302089d894014fbdccff8
Alan Jones <alan.jones@revolt.digital>
2024-01-18 13:23:15 -0300

    Add root route response

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,10 @@ import express from 'express';
 import subRoute from './sub-route'
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send('Hello from root route.');
+});
+
 router.use('/sub', subRoute);
 
 export { router };
